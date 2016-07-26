044c0f2e8cccc8ec9e4ccef696b9ae78dafd746d
johnmberger <johnmartinberger@gmail.com>
2016-07-26 11:14:01 -0600

    added form functionality

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 $(function(){
   console.log('boop!');
 
-  $.ajax({
-    method:   'GET',
-    dataType: 'json',
-    url:      'http://www.omdbapi.com/?t=chef' }).done(function(results) {
-      console.log(results);
-    });
-});
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+    var title = $('#searchBox').val();
+    $.ajax({
+      method:   'GET',
+      dataType: 'json',
+      url:      'http://www.omdbapi.com/?t=' + title, }).done(function(results) {
+        console.log(results);
+      });
+  });
+})
